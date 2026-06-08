f1b82005fffafbbdf49e5ce559aa415c1e0cf9d1
lebit-L1x <lebitl1x@gmail.com>
2026-06-09 02:29:40 +0700

    DAFUQ

---
+++ src/hooks/songs.js
@@ -0,0 +1,44 @@
+export const SONGS = [
+  {
+    id: 1,
+    title: "Answer Me",
+    artist: "imie",
+    musicUrl: "https://piapro.jp/t/6W2N",
+    lyricsUrl: "https://piapro.jp/t/9o24",
+  },
+  {
+    id: 2,
+    title: "After The Curtain",
+    artist: "Rulmry",
+    musicUrl: "https://piapro.jp/t/zoqO",
+    lyricsUrl: "https://piapro.jp/t/EVO2",
+  },
+  {
+    id: 3,
+    title: "Shutter Chance",
+    artist: "Yamiagari",
+    musicUrl: "https://piapro.jp/t/PNpQ",
+    lyricsUrl: "https://piapro.jp/t/wyWv",
+  },
+  {
+    id: 4,
+    title: "The Last March on Earth",
+    artist: "Natsuyama Yotsugi × Dopam!ne",
+    musicUrl: "https://piapro.jp/t/B3yJ",
+    lyricsUrl: "https://piapro.jp/t/9U-6",
+  },
+  {
+    id: 5,
+    title: "Toritsukulogy",
+    artist: "Tsuruzou",
+    musicUrl: "https://piapro.jp/t/QBdL",
+    lyricsUrl: "https://piapro.jp/t/Nixq",
+  },
+  {
+    id: 6,
+    title: "TAKEOVER",
+    artist: "Twinfield",
+    musicUrl: "https://piapro.jp/t/E2i3",
+    lyricsUrl: "https://piapro.jp/t/zxWP",
+  },
+];
