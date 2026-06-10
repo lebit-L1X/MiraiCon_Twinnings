274222a7b9dcf1569a40a8fc4be80347841b3678
lebit-L1x <lebitl1x@gmail.com>
2026-06-11 00:32:19 +0700

    test push pixi stuff

---
+++ src/components/GameCanvas.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react';
+import { Game } from '../pixi/Game';
+
+export const GameCanvas = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const game = new Game();
+    console.log(ref.current);
+    game.init(ref.current);
+
+    return () => {
+
+    };
+  }, []);
+
+  return <div ref={ref} />;
+};
